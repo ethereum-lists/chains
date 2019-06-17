4d489c7a868fa05b7c4520cdb935a62d5cfebae0
Pedro Gomes <pedrogomes94@gmail.com>
2019-06-17 17:45:49 +0200

    add verify-script and run through data

---
+++ script/index.js
@@ -0,0 +1,152 @@
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+const BigNumber = require('bignumber.js')
+
+require('dotenv').config()
+
+const ROOT_DIRECTORY = path.join(__dirname, '../')
+
+const CHAINS_DIRECTORY = path.join(ROOT_DIRECTORY, './_data/chains')
+
+const NET_VERSION_REQ = {
+  id: 1,
+  jsonrpc: '2.0',
+  method: 'net_version',
+  params: []
+}
+
+const CHAIN_ID_REQ = {
+  id: 1,
+  jsonrpc: '2.0',
+  method: 'eth_chainId',
+  params: []
+}
+
+function formatRpcUrl (rpcUrl) {
+  return rpcUrl.replace(
+    '${INFURA_API_KEY}', // eslint-disable-line
+    process.env.INFURA_PROJECT_ID
+  )
+}
+
+async function writeJson (filePath, json) {
+  console.log('Overwriting', filePath)
+  return new Promise((resolve, reject) => {
+    const data = JSON.stringify(json, null, 2)
+    fs.writeFile(filePath, data, (err, res) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(res)
+    })
+  })
+}
+
+async function rpcRequest (rpcUrl, body) {
+  const response = await axios.post(rpcUrl, body, {
+    timeout: 20000, // 20 secs
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+  if (!response && !response.data) {
+    throw new Error('No Response Body')
+  }
+  if (response.data.error && response.data.error.message) {
+    throw new Error(response.data.error.message)
+  }
+  return response.data.result
+}
+
+function toNumber (value) {
+  const BN = new BigNumber(value)
+  let result = null
+  if (!BN.isNaN()) {
+    result = BN.toNumber()
+  }
+  return result
+}
+
+async function getNetworkId (rpcUrl) {
+  try {
+    rpcUrl = formatRpcUrl(rpcUrl)
+    const result = await rpcRequest(rpcUrl, NET_VERSION_REQ)
+    const networkId = toNumber(result)
+    return networkId
+  } catch (error) {
+    return null
+  }
+}
+
+async function getChainId (rpcUrl) {
+  try {
+    rpcUrl = formatRpcUrl(rpcUrl)
+    const result = await rpcRequest(rpcUrl, CHAIN_ID_REQ)
+    const chainId = toNumber(result)
+    return chainId
+  } catch (error) {
+    return null
+  }
+}
+
+fs.readdir(CHAINS_DIRECTORY, function (err, files) {
+  if (err) {
+    console.error('Could not list the directory.', err)
+    process.exit(1)
+  }
+
+  files.forEach(function (file, index) {
+    const filePath = path.join(CHAINS_DIRECTORY, file)
+
+    fs.stat(filePath, async function (error, stat) {
+      if (error) {
+        console.error('Error stating file.', error)
+        return
+      }
+
+      const ext = path.extname(file)
+      if (stat.isFile() && ext === '.json') {
+        const json = require(filePath)
+        console.log('Verifying file', file, 'through file name')
+        const fileName = file.replace(ext, '')
+        if (toNumber(fileName) !== toNumber(json.chainId)) {
+          console.log(`File ${file} chainId doesn't match file name`)
+          json.chainId = toNumber(fileName)
+        }
+
+        if (json.rpc && json.rpc.length) {
+          console.log('Verifying file', file, 'through rpc url')
+          try {
+            const chainIdArr = await Promise.all(
+              json.rpc.map(rpcUrl => getChainId(rpcUrl))
+            ).filter(x => !!x)
+            const chainId =
+              chainIdArr && chainIdArr.length ? chainIdArr[0] : null
+            if (chainId && chainId !== toNumber(json.chainId)) {
+              console.log(`File ${file} chainId doesn't match rpc response`)
+              json.chainId = chainId
+            }
+          } catch (error) {
+            // do nothing
+          }
+          try {
+            const networkIdArr = await Promise.all(
+              json.rpc.map(rpcUrl => getNetworkId(rpcUrl))
+            ).filter(x => !!x)
+            const networkId =
+              networkIdArr && networkIdArr.length ? networkIdArr[0] : null
+            if (networkId && networkId !== toNumber(json.networkId)) {
+              console.log(`File ${file} networkId doesn't match rpc response`)
+              json.networkId = networkId
+            }
+          } catch (error) {
+            // do nothing
+          }
+        }
+        await writeJson(filePath, json)
+      }
+    })
+  })
+})
