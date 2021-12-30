bec23dbc0af1d1e86f4da644ed89c8b09aefaede
ligi <ligi@ligi.de>
2021-12-30 09:00:08 +0100

    Add maven plugin and migrate to kts

---
+++ model/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.google.devtools.ksp").version("1.6.10-1.0.2")
+    id("maven-publish")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            version = "1.0"
+
+            from(components["java"])
+        }
+    }
+}
+
+dependencies {
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
+
+    implementation("com.squareup.moshi:moshi:1.13.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+}
+
