e07d0bd635312f34bb01f70eb06a577b42e6af90
ligi <ligi@ligi.de>
2022-01-14 20:37:26 +0100

    Explorer as enum

--- model/src/main/kotlin/org/ethereum/lists/chains/model/Explorer.kt
+++ model/src/main/kotlin/org/ethereum/lists/chains/model/Explorer.kt
@@ -2,9 +2,14 @@ package org.ethereum.lists.chains.model
 
 import com.squareup.moshi.JsonClass
 
+enum class ExplorerStandard {
+    EIP3091,
+    none
+}
+
 @JsonClass(generateAdapter = true)
 data class Explorer(
     val name: String,
     val url: String,
-    val standard: String
+    val standard: ExplorerStandard
 )
