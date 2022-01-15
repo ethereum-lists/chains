1365ead0acc00a1863561793a0bea17c0ac9d2c8
ligi <ligi@ligi.de>
2022-01-15 10:59:52 +0100

    Add native currency to model

--- model/src/main/kotlin/org/ethereum/lists/chains/model/Chain.kt
+++ model/src/main/kotlin/org/ethereum/lists/chains/model/Chain.kt
@@ -15,7 +15,8 @@ data class Chain(
     val faucets: List<String>,
     val explorers: List<Explorer>?,
     val infoURL: String,
-    val title: String?
+    val title: String?,
+    val nativeCurrency: NativeCurrency
 )
 
 fun List<Chain>.filterEIP3019Explorers() = map { it.copy(explorers = it.explorers?.filterEIP3019()) }
