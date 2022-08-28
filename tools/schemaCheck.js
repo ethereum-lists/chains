f7a27171758094f746485baafc000cbb200b20f0
Ashutosh Kumar <ashutosh@w3dev.in>
2022-08-28 10:05:37 +0000

    Matching ChainID with file name schema.

--- tools/schemaCheck.js
+++ tools/schemaCheck.js
@@ -9,6 +9,10 @@ for(const chainFile of chainFiles){
     const fileLocation = `../_data/chains/${chainFile}`
     const fileData = fs.readFileSync(fileLocation,'utf8')
     const fileDataJson = JSON.parse(fileData)
+    const chainIdFromFileName = chainFile.match(/eip155-(\d+)\.json/)[1]
+    if(chainIdFromFileName != fileDataJson.chainId){
+        throw new Error(`File Name does not match with ChainID in ${chainFile}`)
+    }
     const valid = ajv.validate(schema, fileDataJson)
     if(!valid) {
         console.error(ajv.errors)
