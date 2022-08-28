1953edee97217a3ca35ae852a25fdbcaee233f0a
Ashutosh Kumar <ashutosh@w3dev.in>
2022-08-28 09:52:46 +0000

    improved schemaCheck script

--- tools/schemaCheck.js
+++ tools/schemaCheck.js
@@ -4,17 +4,18 @@ const ajv = new Ajv()
 const schema = require('./schema/chainSchema.json')
 const chainFiles = fs.readdirSync('../_data/chains/');
 
+const filesWithErrors = []
 for(const chainFile of chainFiles){
     const fileLocation = `../_data/chains/${chainFile}`
     const fileData = fs.readFileSync(fileLocation,'utf8')
     const fileDataJson = JSON.parse(fileData)
     const valid = ajv.validate(schema, fileDataJson)
-    const filesWithErrors = []
     if(!valid) {
         console.error(ajv.errors)
         filesWithErrors.push(chainFile)   
     }
-    if(filesWithErrors.length > 0){
-        throw new Error(`Invalid JSON Schema in ${filesWithErrors.length} files at ${filesWithErrors.join(",")}`)
-    }
+}
+
+if(filesWithErrors.length > 0){
+    throw new Error(`Invalid JSON Schema in ${filesWithErrors.length} files at ${filesWithErrors.join(",")}`)
 }
