d34919aa3df4757eb23da2c6ed0403e50558b9ec
r.kumar@edexa.team <r.kumar@edexa.team>
2022-12-21 18:00:05 +0530

    removed new line icons

--- tools/rmNetwork.js
+++ tools/rmNetwork.js
@@ -3,19 +3,19 @@
  * Since this is the only tool here, it is here in index.js
  */
 
-const fs = require('fs');
-const chainFiles = fs.readdirSync('../_data/chains/');
+const fs = require("fs")
+const chainFiles = fs.readdirSync("../_data/chains/")
 
 for (const chainFile of chainFiles) {
-    const fileLocation = `../_data/chains/${chainFile}`
-    const fileData = fs.readFileSync(fileLocation, 'utf8')
-    const fileDataJson = JSON.parse(fileData)
+  const fileLocation = `../_data/chains/${chainFile}`
+  const fileData = fs.readFileSync(fileLocation, "utf8")
+  const fileDataJson = JSON.parse(fileData)
 
-    if (fileDataJson.network) {
-        delete fileDataJson.network
-        fs.writeFileSync(fileLocation, JSON.stringify(fileDataJson, null, 2))
-    }
+  if (fileDataJson.network) {
+    delete fileDataJson.network
+    fs.writeFileSync(fileLocation, JSON.stringify(fileDataJson, null, 2))
+  }
 }
 
-// Note: 
+// Note:
 // Run `npx prettier --write --ignore-unknown _data`from Project Directory
--- tools/schemaCheck.js
+++ tools/schemaCheck.js
@@ -30,12 +30,11 @@ for (const chainFile of chainFiles) {
 }
 
 if (filesWithErrors.length > 0) {
-  filesWithErrors.forEach(file => {
+  filesWithErrors.forEach((file) => {
     console.error(`Invalid JSON Schema in ${file}`)
   })
-  exit(-1);
-}
-else {
-  console.info("Schema check completed successfully");
-  exit(0);
+  exit(-1)
+} else {
+  console.info("Schema check completed successfully")
+  exit(0)
 }
