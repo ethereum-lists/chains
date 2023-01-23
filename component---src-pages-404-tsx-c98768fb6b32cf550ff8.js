fb2980594e9d4740baaf4e0dd0b580171c90b594
ligi <ligi@ligi.de>
2023-01-23 06:26:05 +0000

    Deploying to gh-pages from @ ethereum-lists/chains@12f2a5e510c5cc0e2e0c019fa841921344e11553 🚀

---
+++ component---src-pages-404-tsx-c98768fb6b32cf550ff8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkchainlist=self.webpackChunkchainlist||[]).push([[218],{90938:function(t,e,n){n.r(e);n(67294);var i=n(76541),l=n(23431);e.default=function(){return(0,l.tZ)(i.xu,{p:"8",textAlign:"center"},(0,l.tZ)(i.W2,null,(0,l.tZ)(i.M5,null,(0,l.tZ)(i.X6,{m:"2",as:"h1",size:"2xl"},"404")),(0,l.tZ)(i.xv,{mt:"4",opacity:"0.75"},"These are not the droids you are looking for.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-c98768fb6b32cf550ff8.js.map
