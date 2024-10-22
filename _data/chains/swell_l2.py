d4e854b97987ae08f013f91e9dd603cd5e9a8dae
des0lation <72014077+des0lation@users.noreply.github.com>
2024-10-23 01:01:18 +1100

    Create swell_l2.py

---
+++ _data/chains/swell_l2.py
@@ -0,0 +1,19 @@
+{
+  "name": "Swell L2",
+  "chain": "ETH",
+  "rpc": [
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "features": [],
+  "infoURL": "https://app.swellnetwork.io/layer2/swell-l2",
+  "shortName": "eth",
+  "chainId": 1923,
+  "networkId": 1923,
+  "icon": "ethereum",
+  "explorers": []
+}
