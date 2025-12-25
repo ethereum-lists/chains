9628ef80c8c2fbf542202ff99452a99dd7004c6f
trustburn79-droid <trustburn79@gmail.com>
2025-12-25 23:51:56 +0900

    Create chainid-6000.js

---
+++ chainid-6000.js
@@ -0,0 +1,25 @@
+{
+  "name": "TBURN Mainnet",
+  "chain": "TBURN",
+  "rpc": [
+    "https://tburn.io/rpc"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "TBURN",
+    "symbol": "TBURN",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://tburn.io",
+  "shortName": "tburn",
+  "chainId": 6000,
+  "networkId": 6000,
+  "icon": "tburn",
+  "explorers": [{
+    "name": "TBURNScan",
+    "url": "https://tburn.io/scan",
+    "icon": "tburn",
+    "standard": "EIP3091"
+  }]
+}
