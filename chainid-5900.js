2bcdb40aa0d27d291ae4683cbeb070d2344feea8
trustburn79-droid <trustburn79@gmail.com>
2025-12-25 23:52:35 +0900

    Create chainid-5900.js

---
+++ chainid-5900.js
@@ -0,0 +1,27 @@
+{
+  "name": "TBURN Testnet",
+  "chain": "TBURN",
+  "rpc": [
+    "https://tburn.io/testnet-rpc"
+  ],
+  "faucets": [
+    "https://tburn.io/testnet-scan/faucet"
+  ],
+  "nativeCurrency": {
+    "name": "TBURN",
+    "symbol": "TBURN",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://tburn.io",
+  "shortName": "tburn-testnet",
+  "chainId": 5900,
+  "networkId": 5900,
+  "icon": "tburn",
+  "explorers": [{
+    "name": "TBURNScan Testnet",
+    "url": "https://tburn.io/testnet-scan",
+    "icon": "tburn",
+    "standard": "EIP3091"
+  }]
+}
